906d91253d2702746b66893deeab265ed683b2ee
Haydencccy <135291404+Haydencccy@users.noreply.github.com>
2026-05-14 09:53:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./tests/setup.ts'],
-    css: true,
-    restoreMocks: true,
-    clearMocks: true,
+  server: {
+    host: '0.0.0.0',
+    port: 33030, // 必須同 Nginx proxy_pass 嗰個 port 一樣
+    strictPort: true,
+    allowedHosts: ['hophk.ai-rwa.xyz'],
+    hmr: {
+      host: 'hophk.ai-rwa.xyz',
+      clientPort: 443, // 強烈建議用 clientPort 代替 port: 443，確保 Vite 知道係客端連入嚟嘅 port
+      protocol: 'wss',
+    },
   },
-});
+})
